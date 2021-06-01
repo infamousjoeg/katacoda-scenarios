3de85528650ff722e03687fbf767ab2fb24f7ad7
Joe Garcia <joe@joe-garcia.com>
2021-06-01 13:20:26 -0400

    update background init

--- impact2021-security-workshop_webapp/background.sh
+++ impact2021-security-workshop_webapp/background.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 
+docker run --name lamp -d -p "80:80" -v ${PWD}/app:/app mattrayner/lamp:latest-1804
+docker exec lamp mysql -h localhost -uroot \
+    -e "CREATE DATABASE conjur_demo; CREATE USER 'devapp1' IDENTIFIED BY 'Cyberark1'; GRANT ALL PRIVILEGES ON conjur_demo.* TO 'devapp1'; FLUSH PRIVILEGES;"
+curl -fsSL cybr.rocks/conjur-install | bash -s
